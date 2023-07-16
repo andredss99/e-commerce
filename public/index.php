05c3597dfb8be7d50dcd2ee86e5c623fcf7b7a2f
André Sousa <andresousa808@gmail.com>
2023-07-16 04:08:43 -0300

    feat: added catalog template files and resources

--- public/index.php
+++ public/index.php
@@ -4,21 +4,30 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Selective\BasePath\BasePathMiddleware;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$twig = Twig::create('../view/Catalog/', [
+    'cache' => false
+]);
 
 $app = AppFactory::create();
 
 $app->addRoutingMiddleware();
 
 $app->add(new BasePathMiddleware($app));
+$app->add(TwigMiddleware::create($app, $twig));
 
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $payload = 'Hello, world!';
-    $response->getBody()->write($payload);
-    return $response;
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'index.twig');
+//    $payload = 'Hello, world!';
+//    $response->getBody()->write($payload);
+    //return $response;
 });
 
 $app->run();
