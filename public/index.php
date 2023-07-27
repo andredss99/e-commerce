dd87c5ff757304b765faa7f3d1850c4489db44c1
André Sousa <andresousa808@gmail.com>
2023-07-27 18:21:19 -0300

    feat: added session and new admin routes

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use Ecommerce\View;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Selective\BasePath\BasePathMiddleware;
@@ -7,14 +8,18 @@ use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 
+session_start();
+
 require __DIR__ . '/../vendor/autoload.php';
 
-$twig = Twig::create('../view/Catalog/', [
-    'cache' => false
+$twig = Twig::create('../view/', [
+    'cache' => '../view/cache/',
+    'debug' => true
 ]);
 
 $app = AppFactory::create();
 
+$app->addBodyParsingMiddleware();
 $app->addRoutingMiddleware();
 
 $app->add(new BasePathMiddleware($app));
@@ -22,12 +27,17 @@ $app->add(TwigMiddleware::create($app, $twig));
 
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $view = Twig::fromRequest($request);
-    return $view->render($response, 'index.twig');
-//    $payload = 'Hello, world!';
-//    $response->getBody()->write($payload);
-    //return $response;
+$app->get('/', [\Ecommerce\Controller\HomeController::class, 'index']);
+
+$app->get('/admin', [\Ecommerce\Controller\AdminController::class, 'index']);
+
+$app->get('/admin/login', function (Request $request, Response $response) {
+    $template = new View(View::VIEW_TYPE_ADMIN, $request, $response);
+    return $template->renderView('login.twig');
 });
+
+$app->get('/admin/logout', [\Ecommerce\Controller\AdminController::class, 'logout']);
+
+$app->post('/admin/login', [\Ecommerce\Controller\AdminController::class, 'login']);
 
 $app->run();
