7de645f816735edc991f4ffd2ce79995a76f5415
André Sousa <andresousa808@gmail.com>
2023-07-27 15:38:31 -0300

    refactor: removed unused methods

--- src/Infrastructure/DB/DbConnection.php
+++ src/Infrastructure/DB/DbConnection.php
@@ -4,68 +4,26 @@ namespace Ecommerce\Infrastructure\DB;
 
 use PDO;
 
-class DbConnection {
+class DbConnection
+{
     private const HOSTNAME = '172.19.0.2';
     private const USERNAME = 'andre';
     private const PASSWORD = 'password';
     private const DB_NAME = 'db_ecommerce';
 
-	private PDO $conn;
+    private PDO $conn;
 
-	public function __construct()
-	{
+    public function __construct()
+    {
         $this->conn = new PDO(
             'mysql:dbname=' . self::DB_NAME . ';host=' . self::HOSTNAME,
             self::USERNAME,
             self::PASSWORD
         );
-	}
-
-	private function setParams($statement, $parameters = array())
-	{
-
-		foreach ($parameters as $key => $value) {
-			
-			$this->bindParam($statement, $key, $value);
-
-		}
-
-	}
-
-	private function bindParam($statement, $key, $value)
-	{
-
-		$statement->bindParam($key, $value);
-
-	}
-
-	/*public function query($rawQuery, $params = array())
-	{
-
-		$stmt = $this->conn->prepare($rawQuery);
-
-		$this->setParams($stmt, $params);
-
-		$stmt->execute();
-
-	}*/
-
-	public function select($rawQuery, $params = array()):array
-	{
-
-		$stmt = $this->conn->prepare($rawQuery);
-
-		$this->setParams($stmt, $params);
-
-		$stmt->execute();
-
-		return $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-	}
+    }
 
     public function getConnection(): PDO
     {
         return $this->conn;
     }
-
 }
